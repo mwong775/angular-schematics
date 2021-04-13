c1edf540ebaa319f6be58df98989916184e3afe5
Melanie <melywong@ucsc.edu>
2021-04-12 17:16:35 -0700

    no more splicing

--- src/shared/statements-adder.ts
+++ src/shared/statements-adder.ts
@@ -1,6 +1,10 @@
 import { SchematicsException, Tree } from '@angular-devkit/schematics';
 import { InsertChange } from '@schematics/angular/utility/change';
 
+export interface IHash {
+  [details: string] : number;
+}
+
 export function AddStatements(filePath: string, labels: string[], statements: string[], tree: Tree) {
     let text = tree.read(filePath); // reads the file from the tree
     if (!text) {
@@ -9,14 +13,22 @@ export function AddStatements(filePath: string, labels: string[], statements: st
     let sourceText = text.toString('utf-8');
     let index = 0;
     const exportRecorder = tree.beginUpdate(filePath);
+    let labelMap : IHash = {};
     for (let i = 0; i < labels.length; i++) {
-      if (i)
-        sourceText = sourceText.slice(index);
-      index = index + sourceText.indexOf(labels[i]) + labels[i].length;
+      // sourceText = sourceText.slice(index);
+      if (labelMap[labels[i]] === undefined)
+          labelMap[labels[i]] = 0;
+      labelMap[labels[i]]++;
+      let count = labelMap[labels[i]];
+      index = getPosition(sourceText, labels[i], count) + labels[i].length;
       // declares import
       const insertChange = new InsertChange(filePath, index, `\n${statements[i]}`);
       exportRecorder.insertLeft(insertChange.pos, insertChange.toAdd);
     }
     tree.commitUpdate(exportRecorder); 
   
+  }
+
+  function getPosition(string: string, subString: string, index: number) {
+    return string.split(subString, index).join(subString).length;
   }
